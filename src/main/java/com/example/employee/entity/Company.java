7847e23505fc2324b73e51a8e003128cb5a1775e
beamrolling <beamrolling@outlook.com>
2019-07-07 16:12:49 +0800

    Complete entity class.

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,47 @@
 package com.example.employee.entity;
 
-public class Company {
+/*
+ * 实体Employee包含字段分别有：id, 姓名(name), 年龄(age), 性别(gender), companyId, 薪资(salary)
+ * 实体Company包含字段分别有：id, 公司名(companyName), 员工数量(employeesNumber)
+ */
 
+import javax.persistence.Id;
+
+public class Company {
+    @Id
+    private int id;
+    private String companyName;
+    private int employeesNumber;
+
+    public Company() {}
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,79 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+/*
+ * 实体Employee包含字段分别有：id, 姓名(name), 年龄(age), 性别(gender), companyId, 薪资(salary)
+ * 实体Company包含字段分别有：id, 公司名(companyName), 员工数量(employeesNumber)
+ */
+@Entity
+public class Employee {
+    @Id
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public Employee() {}
+
+    public Employee(int id, String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
